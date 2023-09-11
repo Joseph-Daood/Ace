2f89633b74996690d4876023ad20fc1e45184a41
Joseph-Daood <josephdaood@gmail.com>
2023-09-11 12:47:08 +0200

    Add meta data to the api response for pagenation

---
+++ Ace.Shared/Helpers/ResourceUri.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ace.Shared.Helpers
+{
+    public static class ResourceUri
+    {
+        public static string? CreateResourceUri(IUrlHelper url,
+       ResourceParameters.ResourceParameters resourceParameters,
+       ResourceUriType type, string targethttpRequestName)
+        {
+            switch (type)
+            {
+                case ResourceUriType.PreviousPage:
+                    return url.Link(targethttpRequestName,
+                        new
+                        {
+                            pageNumber = resourceParameters.PageNumber - 1,
+                            pageSize = resourceParameters.PageSize,
+                            mainCategory = resourceParameters.MainCategory,
+                            searchQuery = resourceParameters.SearchQuery
+                        });
+                case ResourceUriType.NextPage:
+                    return url.Link(targethttpRequestName,
+                        new
+                        {
+                            pageNumber = resourceParameters.PageNumber + 1,
+                            pageSize = resourceParameters.PageSize,
+                            mainCategory = resourceParameters.MainCategory,
+                            searchQuery = resourceParameters.SearchQuery
+                        });
+                default:
+                    return url.Link(targethttpRequestName,
+                        new
+                        {
+                            pageNumber = resourceParameters.PageNumber,
+                            pageSize = resourceParameters.PageSize,
+                            mainCategory = resourceParameters.MainCategory,
+                            searchQuery = resourceParameters.SearchQuery
+                        });
+            }
+        }
+    }
+}
